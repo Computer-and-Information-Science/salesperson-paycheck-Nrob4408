e0fd5199ae2c72e58996c3bd987134c1c7158257
Nrob4408 <nate.roberts@student.northampton.edu>
2026-02-09 20:12:42 +0000

    paycheck assignment revision

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
+// Nate Roberts
+
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+
 using namespace std;
 
 int main() {
  
   string firstName;
-  string lastName;
+  string lastName, fileName;
   double baseSalary;
   double commissionPercent;
   double employeeTotalSales;
   double employeeExpenses;
   double commission;
   double total;
+  ifstream readData;
 
+  cout << "Enter Employee's File: " << endl;
+cin >> fileName;
   
-cin >> firstName >> lastName;
-cin >> baseSalary >> commissionPercent;
-cin >> employeeTotalSales;
-cin >> employeeExpenses;
+  readData.open(fileName);
+
+  readData >> firstName >> lastName >> baseSalary >> commissionPercent >> employeeTotalSales >> employeeExpenses;
 
 commission =((commissionPercent/100)*(employeeTotalSales));
 total = baseSalary + commission + employeeExpenses;
 
 
 
-cout << "Payroll data for " << fixed << setprecision(2) << " " << lastName << endl;
+cout << "Payroll data for " << fixed << setprecision(2) << firstName << " " << lastName << endl;
 cout << "   " << endl;
 cout << "Base Salary: " <<  setw(11) << baseSalary << endl;
 cout << "Commission: " <<  setw(12) << commission << " ("<< commissionPercent << "%" << " of " << employeeTotalSales << ")" << endl;
